71a7ac2d4200ba9f0199236dc0ea3099e8e623be
Aaron LeMoine <aaronsr@gmail.com>
2020-08-12 16:35:57 -0700

    Update testFixtures.py

--- custcomms/tests/unit/testFixtures.py
+++ custcomms/tests/unit/testFixtures.py
@@ -7,14 +7,14 @@ def apigw_event():
 
     return {
         "body": {
-          "token": "n3TVwSTLyNB9CJp8RS0ljdIB",
-          "team_id": "T0BEPSLJV",
+          "token": "Token",
+          "team_id": "TeamID",
           "api_app_id": "AL3RKF63V",
           "event": {
             "client_msg_id": "7be085f6-d527-4015-a165-a75ae24ed7d9",
             "type": "message",
             "text": "Create Notification Test",
-            "user": "UH63D6AP9",
+            "user": "UserID",
             "ts": "1567180526.034200",
             "team": "T0BEPSLJV",
             "channel": "DLEQERQP6",
@@ -22,10 +22,10 @@ def apigw_event():
             "channel_type": "im"
           },
           "type": "event_callback",
-          "event_id": "EvMJHA6U9Y",
-          "event_time": "1567180526",
+          "event_id": "",
+          "event_time": "",
           "authed_users": [
-            "ULHD2RDJA"
+            ""
           ]
         },
         "headers": {
@@ -43,11 +43,11 @@ def apigw_event():
           "Via": "1.1 ade18dc841d2e1cc8ef49611c5d4c93e.cloudfront.net (CloudFront)",
           "X-Amz-Cf-Id": "sahQqO9KtqtOHSAQC67wNMYxKMrVGHsbuj1PMz2Jd0B_d9vifBESNw==",
           "X-Amzn-Trace-Id": "Root=1-5d6946ee-4ecf6c8afbdb4f661468c93a",
-          "X-Forwarded-For": "54.158.77.65, 70.132.60.89",
+          "X-Forwarded-For": "",
           "X-Forwarded-Port": "443",
           "X-Forwarded-Proto": "https",
-          "X-Slack-Request-Timestamp": "1567180526",
-          "X-Slack-Signature": "v0=355ea209964ab407d2fe99ea6ce3389889f8238aebe3c660a79d30f3bb1a9a5d"
+          "X-Slack-Request-Timestamp": "",
+          "X-Slack-Signature": ""
         }
       }
 
@@ -58,41 +58,41 @@ def return_slack_message_parse_data():
         'client_msg_id': '7be085f6-d527-4015-a165-a75ae24ed7d9',
         'type': 'message',
         'text': 'Create Notification Test',
-        'user': 'UH63D6AP9',
+        'user': '',
         'ts': '1567180526.034200',
-        'team': 'T0BEPSLJV',
-        'channel': 'DLEQERQP6',
+        'team': '',
+        'channel': '',
         'event_ts': '1567180526.034200',
         'channel_type': 'im'
       },
       {
         'ok': True,
         'user': {
-          'id': 'UH63D6AP9',
-          'team_id': 'T0BEPSLJV',
-          'name': 'alemoine',
+          'id': '',
+          'team_id': '',
+          'name': '',
           'deleted': False,
           'color': 'db3150',
-          'real_name': 'Aaron LeMoine',
-          'tz': 'America/Phoenix',
-          'tz_label': 'Mountain Time',
+          'real_name': '',
+          'tz': '',
+          'tz_label': '',
           'tz_offset': -25200,
           'profile': {
             'title': '',
             'phone': '',
             'skype': '',
-            'real_name': 'Aaron LeMoine',
-            'real_name_normalized': 'Aaron LeMoine',
-            'display_name': 'alemoine',
-            'display_name_normalized': 'alemoine',
+            'real_name': '',
+            'real_name_normalized': '',
+            'display_name': '',
+            'display_name_normalized': '',
             'status_text': '',
             'status_emoji': '',
             'status_expiration': 0,
             'avatar_hash': 'f372b9fd917a',
             'image_original': 'https://avatars.slack-edge.com/2019-04-01/595736632548_f372b9fd917afaa1424f_original.jpg',
-            'email': 'alemoine@istreamplanet.com',
-            'first_name': 'Aaron',
-            'last_name': 'LeMoine',
+            'email': '',
+            'first_name': '',
+            'last_name': '',
             'image_24': 'https://avatars.slack-edge.com/2019-04-01/595736632548_f372b9fd917afaa1424f_24.jpg',
             'image_32': 'https://avatars.slack-edge.com/2019-04-01/595736632548_f372b9fd917afaa1424f_32.jpg',
             'image_48': 'https://avatars.slack-edge.com/2019-04-01/595736632548_f372b9fd917afaa1424f_48.jpg',
@@ -114,13 +114,13 @@ def return_slack_message_parse_data():
           'updated': 1567525379
         }
       },
-      'alemoine@istreamplanet.com',
+      '**EMailAddress',
       [
         'create',
         'notification',
         'test'
       ],
-      'DLEQERQP6')
+      '')
 
 
 @pytest.fixture()
